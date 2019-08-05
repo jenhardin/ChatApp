52098fbf9008451fabd3bfcb5bba4b4e04a83682
Jennifer Hardin <48570664+jenhardin@users.noreply.github.com>
2019-08-05 12:14:46 -0700

    test

--- justchat/justchat/routing.py
+++ justchat/justchat/routing.py
@@ -11,4 +11,25 @@ application = ProtocolTypeRouter({
         )
     ),
 })
+'''
+from channels.routing import ProtocolTypeRouter
 
+
+class CustomSocketioProtocolTypeRouter(ProtocolTypeRouter):
+    """
+    Overrided base class's __call__ method to support python socketio 4.2.0 and daphne 2.3.0
+    """
+    def __call__(self, scope, *args):
+        if scope["type"] in self.application_mapping:
+            handlerobj = self.application_mapping[scope["type"]](scope)
+            if args:
+                return handlerobj(*args)
+            return handlerobj
+        raise ValueError(
+            "No application configured for scope type %r" % scope["type"]
+        )
+
+application = CustomSocketioProtocolTypeRouter({
+    # (http->django views is added by default)
+})
+'''
